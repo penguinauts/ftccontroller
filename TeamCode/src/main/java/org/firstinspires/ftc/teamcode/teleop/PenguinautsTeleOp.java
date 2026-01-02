34f624ed31800628f092bec768a77f9d165abd16
penguinauts <penguinauts.ftc@gmail.com>
2026-01-02 17:02:11 -0600

    Vineela is testing push revert

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PenguinautsTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PenguinautsTeleOp.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
